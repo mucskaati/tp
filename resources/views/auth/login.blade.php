351e34754b5cbf21f1e7d52efd4f317ecae49d71
Tomáš Drus <tomasdrus@gmail.com>
2022-03-11 02:11:44 +0100

    refactor: layouts to plain.blade and app.blade contains navbar + footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,47 @@
-@extends('layouts.main')
+@extends('layouts.plain')
 @section('title', 'Prihlásenie')
 @section('content')
 
-    <div class="mx-auto md:h-screen flex flex-col justify-center items-center px-6 pt-8 pt:mt-0">
-        <div class="bg-white shadow rounded-lg md:mt-0 w-full sm:max-w-md p-10">
-            <h1 class="text-2xl text-center font-bold text-gray-900">Prihlásenie</h1>
+    <div class="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
+        <div class="w-full p-10 bg-white rounded-lg shadow md:mt-0 sm:max-w-md">
+            <h1 class="text-2xl font-bold text-center text-gray-900">Prihlásenie</h1>
             <form class="mt-8 space-y-6" method="post" action="{{ route('login') }}">
                 {{ csrf_field() }}
 
                 @if (isset($message))
-                    <div class="flex justify-between items-center">
+                    <div class="flex items-center justify-between">
                         <div class="flex items-center text-red">
                             {{ $message }}
                         </div>
                     </div>
                 @endif
                 <div>
-                    <label for="email" class="text-sm font-medium text-gray-900 block mb-2">Prihlasovacie meno</label>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Prihlasovacie meno</label>
                     <input type="text" name="username" id="email" placeholder="usernick"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                 </div>
 
                 <div>
-                    <label for="password" class="text-sm font-medium text-gray-900 block mb-2">Heslo</label>
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Heslo</label>
                     <input type="password" name="password" id="password" placeholder="••••••••"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                 </div>
 
-                <div class="flex justify-between items-center">
+                <div class="flex items-center justify-between">
                     <div class="flex items-center">
-                        <!-- <input id="remember" name="remember" type="checkbox" class="h-4 w-4">
-                                                                                <label for="remember" class="text-sm font-medium text-gray-700 ml-2">Zapamätať prihlásenie</label> -->
+                        <!-- <input id="remember" name="remember" type="checkbox" class="w-4 h-4">
+                                                                                <label for="remember" class="ml-2 text-sm font-medium text-gray-700">Zapamätať prihlásenie</label> -->
                     </div>
-                    <a href="/forgotten-password" class="text-sm font-medium hover:underline text-gray-700">Zabudnuté
+                    <a href="/forgotten-password" class="text-sm font-medium text-gray-700 hover:underline">Zabudnuté
                         heslo</a>
                 </div>
 
                 <button type="submit"
-                    class="text-white bg-cyan-600 hover:bg-cyan-700 font-medium rounded-lg py-2 w-full text-center">Prihlásenie</button>
+                    class="w-full py-2 font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700">Prihlásenie</button>
 
                 <div class="flex justify-between">
                     <a href="/registration" class="text-sm font-medium hover:underline text-cyan-600">Registrácia</a>
-                    <a href="/" class="text-sm font-medium hover:underline text-gray-700">Domov</a>
+                    <a href="/" class="text-sm font-medium text-gray-700 hover:underline">Domov</a>
                 </div>
             </form>
         </div>
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,44 +1,44 @@
-@extends('layouts.main')
+@extends('layouts.plain')
 @section('title', 'Registrácia')
 @section('content')
 
-    <div class="mx-auto md:h-screen flex flex-col justify-center items-center px-6 pt-8 pt:mt-0">
-        <div class="bg-white shadow rounded-lg md:mt-0 w-full sm:max-w-md p-10">
-            <h1 class="text-2xl text-center font-bold text-gray-900">Registrácia</h1>
+    <div class="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
+        <div class="w-full p-10 bg-white rounded-lg shadow md:mt-0 sm:max-w-md">
+            <h1 class="text-2xl font-bold text-center text-gray-900">Registrácia</h1>
             <form class="mt-8 space-y-6" method="POST" action="{{ route('register') }}">
                 {{ csrf_field() }}
 
                 @if (isset($message))
-                    <div class="flex justify-between items-center">
+                    <div class="flex items-center justify-between">
                         <div class="flex items-center text-red">
                             {{ $message }}
                         </div>
                     </div>
                 @endif
                 <div>
-                    <label for="username" class="text-sm font-medium text-gray-900 block mb-2">Uživateľské meno<span
-                            class="text-red-600 text-sm">*</span></label>
+                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Uživateľské meno<span
+                            class="text-sm text-red-600">*</span></label>
                     <input type="username" name="username" id="username" placeholder="nickname"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                 </div>
 
                 <div>
-                    <label for="password" class="text-sm font-medium text-gray-900 block mb-2">Heslo <span
-                            class="text-red-600 text-sm">*</span></label>
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Heslo <span
+                            class="text-sm text-red-600">*</span></label>
                     <input type="password" name="password" id="password" placeholder="••••••••"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                 </div>
 
-                <p class="text-gray-700 text-sm"><span class="text-red-600 text-sm">*</span> označené polia sú povinné</p>
+                <p class="text-sm text-gray-700"><span class="text-sm text-red-600">*</span> označené polia sú povinné</p>
 
                 <button type="submit"
-                    class="text-white bg-cyan-600 hover:bg-cyan-700 font-medium rounded-lg py-2 w-full text-center">Zaregistrovať
+                    class="w-full py-2 font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700">Zaregistrovať
                     sa</button>
 
                 <div class="flex justify-between">
                     <a href="{{ route('login') }}"
                         class="text-sm font-medium hover:underline text-cyan-600">Prihlásenie</a>
-                    <a href="{{ route('dashboard') }}" class="text-sm font-medium hover:underline text-gray-700">Domov</a>
+                    <a href="{{ route('dashboard') }}" class="text-sm font-medium text-gray-700 hover:underline">Domov</a>
                 </div>
             </form>
         </div>
