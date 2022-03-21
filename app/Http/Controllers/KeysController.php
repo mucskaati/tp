0a6b5657e1cd4ab2554808ed054c456dc73d53ae
Ati <attila.mucska@gmail.com>
2022-03-21 12:19:33 +0100

    todo: adding keys with images - guzzle

--- app/Http/Controllers/KeysController.php
+++ app/Http/Controllers/KeysController.php
@@ -34,18 +34,40 @@ class KeysController extends Controller
 
         // dd($va);
 
-        $res = Http::withHeaders([
-            'authorization' => loggedUser()['token'],
-        ])
-            ->accept('application/json');
+        $image_path = $request->file('nomenclatorImage')->getPathname();
+        $image_mime = $request->file('nomenclatorImage')->getmimeType();
+        $image_org  = $request->file('nomenclatorImage')->getClientOriginalName();
 
-        if ($request->has('nomenclatorImage')) $res = $res->attach('nomenclatorImage', $request->file('nomenclatorImage'), 'nomenclatorImage.jpg');
-            
-        $res->post($this->api_base_url . '/nomenclatorKeys', $va);
+        $va['nomenclatorImages'] =
+            [
+                'name'     => 'image',
+                'filename' => $image_org,
+                'Mime-Type' => $image_mime,
+                'contents' => $request->file('nomenclatorImage')->get(),
+            ];
 
-        $keys = $res->json();
+        dd($va);
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', $this->api_base_url . '/nomenclatorKeys', [
+            'headers' => [
+                'authorization' => loggedUser()['token']
+            ],
+            'multipart' => $va,
+        ]);
 
-        dd($res, $keys, $va);
+        // $res = Http::acceptJson()->attach('attachment', file_get_contents($request->file('nomenclatorImage'), 'nomecnaltor.jpg'))->withHeaders([
+        //     'authorization' => loggedUser()['token'],
+        // ])
+        //     ->accept('application/json');
+
+        // // if ($request->has('nomenclatorImage')) $res = $res->attach('nomenclatorImage', $request->file('nomenclatorImage'), 'nomenclatorImage.jpg');
+
+        // $res->post($this->api_base_url . '/nomenclatorKeys', $va);
+
+        //keys = $res->json();
+
+        dd($response->getBody());
+
 
         return view('nomenclator.create');
     }
